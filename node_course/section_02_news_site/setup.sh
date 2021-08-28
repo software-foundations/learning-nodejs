5ee16d41aa1c6b6e0b4f8b241d606c349b0564aa
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-08-28 10:53:38 -0300

    [node_course] -> (npm_setup.sh, at /; setup.sh, at section_02_news_site/) -> 18: installing and testing nodemon

--- node_course/section_02_news_site/setup.sh
+++ node_course/section_02_news_site/setup.sh
@@ -1,12 +1,14 @@
+# -> Start npm project
 npm init
 
-# use --save to save in package.json (module project)
+# -> Use --save to save in package.json (module project)
 npm install express --save
 
 # -> To create dinamic html pages
 npm install ejs@2.5.6 --save
 
-# -> node monitoring
+# -> Node monitoring
+# -g to set this module globally
 npm install -g nodemon@1.10.2
 
 # 3 -> using nodemon
