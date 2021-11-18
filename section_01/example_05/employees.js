2113980992979a7118e29e21c8ad835d8f5b2457
brunoconde07 <brunoconde.ufpa@gmail.com>
2021-11-17 21:57:47 -0300

    [javascript-coder] -> 155: solving chalenge (using .filter and .reduce in a json obtained from get http request) -> [modified] (employees.js, at /section_01/example_05/)

--- section_01/example_05/employees.js
+++ section_01/example_05/employees.js
@@ -2,7 +2,24 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 
 const axios = require('axios')
 
+const chineses = employees => employees.pais == "China"
+
+const women = employees => employees.genero == "F"
+
+const lowestSalary = (employee, currentEmployee) => {
+
+    return employee.salario < currentEmployee.salario ? employee : currentEmployee
+}
+
 axios.get(url).then(response => {
     const employees = response.data
+
     console.log(employees)
+
+    const employee = employees
+        .filter(chineses)
+        .filter(women)
+        .reduce(lowestSalary)
+
+    console.log(employee)
 })
