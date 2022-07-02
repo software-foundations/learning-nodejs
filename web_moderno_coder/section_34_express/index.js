77c5fed2d8cd95080303784321d741e32e051be5
brunoconde07 <brunoconde.ufpa@gmail.com>
2022-07-02 11:46:08 -0300

    [web-moderno-coder-express] -> class 603

--- web_moderno_coder/section_34_express/index.js
+++ web_moderno_coder/section_34_express/index.js
@@ -17,6 +17,24 @@ app.get('/hello-get', (req, res) => {
 	res.send('Hello <b>get</b>')
 })
 
+app.get('/hello-get-json', (req, res) => {
+	res.json({name: 'Hello from json'})
+})
+
+app.get('/hello-get-array-json', (req, res) => {
+	res.json({
+		data: [
+			{name: 'Bruno', status: 'on'},
+			{name: 'John', status: 'off'}
+		],
+		// useful for pagination
+		count: 20,
+		skip: 0,
+		limit: 2,
+		status: 200
+	})
+})
+
 app.listen(3000, () => {
 	console.log('Server running on port 3000')
 })
